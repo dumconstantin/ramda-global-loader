e8df401d720df6147d509c90726ad0fc50d9acb0
dumconstantin <dum.constantin@gmail.com>
2016-04-21 17:38:36 +0300

    Implement AST parsing and selecting Ramda functions

--- src/loader.js
+++ src/loader.js
@@ -1,10 +1,30 @@
-import ramda from 'ramda'
+import * as R from 'ramda'
+import { traverse } from 'estraverse'
+import ast from 'ast'
+import * as acorn from 'acorn'
 
 module.exports = function (source, map) {
 
   this.cacheable()
 
-  source = `import { ${Object.keys(ramda).join(', ') } } from 'ramda'\n${source}`
+  let fns = []
+  let ramdaFn = Object.keys(R)
+  var ast = acorn.parse(source)
+
+  traverse(ast, {
+    enter(node, parent) {
+      if (-1 !== ramdaFn.indexOf(node.name)) {
+        if (undefined === parent.object && 'FunctionDeclaration' !== parent.type) {
+          fns.push(node.name)
+        }
+      }
+      return node
+    }
+  })
+
+  let header = 'var R = require("ramda")\n'
+  header += R.join('\n', R.map(x => `var ${x} = R.${x}`, fns))
+  source = header + '\n\n' + source
 
   this.callback(null, source, map)
 
